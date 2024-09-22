26c1817638df2fdc14ffa49cc8d7649691ae8377
Nishkarsh Sahu <nishkarshsahu007@gmail.com>
2024-09-22 17:43:52 +0530

    profile picture work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,28 @@ class UsersController < ApplicationController
 		@posts = @user.posts 
 	end
 
+	def edit
+		@user = User.find_by(id: params[:id])
+	end
+
+	def update
+	  @user = User.find(params[:id]) 
+
+	  if @user.update(user_params)
+	    @posts = @user.posts
+	    redirect_to @user, notice: "User updated successfully!"
+	  else
+	    render :edit  # Re-render the edit form if update fails
+	  end
+	end
+
 	def profiles
 		@profiles = User.all 
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:username, :first_name, :last_name, :email, :password, :image)
 	end
 end
